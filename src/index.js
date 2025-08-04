0eacf8abc9a7e13a0c284cdb57a7ab32471192d0
Noa <noahoffner@gmail.com>
2025-08-04 03:11:50 +0300

    Added react-router to handle signing page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import SignPage from './SignPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/sign/:fileId" element={<SignPage />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/sign/:fileId" element={<SignPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
